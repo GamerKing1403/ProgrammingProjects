77d9c13e9f44bd77af91f164192483c75a3ee418
TusharJain <Tusharjain1903@gmail.com>
2021-01-17 22:04:40 +0530

    Changed Selenium to bs4

--- PycharmProjects/piMessageSender/onePieceChapterNotifier.py
+++ PycharmProjects/piMessageSender/onePieceChapterNotifier.py
@@ -1,32 +1,32 @@
 import json
 import requests
 from datetime import datetime as dt
+from bs4 import BeautifulSoup
 import time
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 
 
 slack_token = 'xoxb-1546263022019-1569905814384-VwVfQLvSwSUpZ9OWtdV6GyHb'
 slack_channel = '#notifications-from-computers'
 slack_user_name = 'Notifications'
 
-
 today = dt.today()
 currentDay = dt.date(today).isocalendar()
 nxt_sunday_date = today.day + (7 - currentDay[2])
 nxt_sunday = dt(year=today.year, month=today.month, day=nxt_sunday_date, hour=22, minute=25, second=0)
 
+headersToUse = {'User-Agent': 'FooBar-Spider 1.0'}
+urlToUse = 'https://claystage.com/one-piece-chapter-release-schedule-for-{}'.format(currentDay[0])
+value = None
 
-chromeOptions = webdriver.ChromeOptions()
-chromeOptions.add_argument('--headless')
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=chromeOptions)
-driver.get('https://claystage.com/one-piece-chapter-release-schedule-for-{}'.format(currentDay[0]))
-value = driver.find_element_by_xpath(
-    "/html/body/div[1]/div/main/div/div/div/article/div/div[3]/table/tbody/tr[{}]/td[2]".format(currentDay[1])).text
+
+def getPage(url, headers):
+    r = requests.get(url, headers=headers)
+    r.raise_for_status()
+    return BeautifulSoup(r.text, 'html.parser').find_all('td', class_='col-2')[currentDay[1]-1].text
 
 
 def sendMessageOnTime():
-    global today
+    global today, value
     today = dt.today()
     difference = (nxt_sunday - today)
     total_seconds = difference.total_seconds()
@@ -35,6 +35,7 @@ def sendMessageOnTime():
     if currentHour == 22 and 25 <= currentMin <= 30 and currentDay[2] == 7:
         Message = 'New One Piece Chapter in {} mins'.format(30 - currentMin)
         post_message_to_slack(Message)
+        value = getPage(urlToUse, headersToUse)
     else:
         print('going to sleep for ', total_seconds)
         time.sleep(total_seconds)
@@ -51,9 +52,15 @@ def post_message_to_slack(text, blocks=None):
     }).json()
 
 
-if __name__ == '__main__':
+def main():
+    global value
     try:
+        value = getPage(urlToUse, headersToUse)
         int(value)
         sendMessageOnTime()
     except ValueError as e:
-        pass
+        time.sleep(600000)
+
+
+if __name__ == '__main__':
+    main()
--- PycharmProjects/piMessageSender/test.py
+++ PycharmProjects/piMessageSender/test.py
@@ -1,12 +1,14 @@
+# import requests
+# from bs4 import BeautifulSoup
 from datetime import datetime as dt
-import datetime
+# headers = {'User-Agent': 'FooBar-Spider 1.0'}
+# url = 'https://claystage.com/one-piece-chapter-release-schedule-for-{}'.format(2021)
+# r = requests.get(url, headers=headers)
+# r.raise_for_status()
+# soup = BeautifulSoup(r.text, 'html.parser')
+# soup.find_all('td', class_='col-2')[1].text
+
 
 today = dt.today()
 currentDay = dt.date(today).isocalendar()
-nxt_sunday_date = today.day + (7 - currentDay[2])
-nxt_sunday = dt(year=today.year, month=today.month, day=16, hour=23, minute=50, second=0)
-print(today)
-print(nxt_sunday)
-difference = (nxt_sunday - today)
-total_seconds = difference.total_seconds()
-print(total_seconds)
+print(currentDay)
